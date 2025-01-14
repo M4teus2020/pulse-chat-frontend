750d62fc458186eb85fce0f58d5a6822536e818f
Mateus Felini <mfelini7@gmail.com>
2025-01-13 22:07:30 -0300

    ✨ feat: add ContainerComponent and update utility functions
    - Introduced ContainerComponent.vue for flexible layout management with customizable classes.
    - Added clsx and tailwind-merge as dependencies for improved class management in the project.

---
+++ src/components/ui/ContainerComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { cn } from '@/utils';
+import type { ClassValue } from 'clsx';
+
+const props = defineProps<{
+  class?: ClassValue
+  headerClass?: ClassValue
+}>()
+</script>
+
+<template>
+  <div
+    :class="cn('flex flex-col rounded-2xl bg-surface-50 p-5 dark:bg-surface-900', props.class)"
+  >
+    <slot>
+      <div :class="cn('h-10', props.headerClass)">
+        <slot name="header" />
+      </div>
+
+      <Divider />
+
+      <slot name="body" />
+    </slot>
+  </div>
+</template>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,17 @@
 import type { ApiError } from '@/types/error'
+import clsx, { type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 import { isAxiosError } from 'axios'
 
-export function getApiError(
-  error: unknown,
-): ApiError | null {
+export function getApiError(error: unknown): ApiError | null {
   if (!isAxiosError(error)) return null
   if (!error.response) return null
   if (typeof error.response.data !== 'object') return null
   if (!('message' in error.response.data)) return null
 
   return error.response.data as ApiError
+}
+
+export function cn(...classes: ClassValue[]) {
+  return twMerge(clsx(...classes))
 }
